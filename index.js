2e7f3c0a5acb51e7f5ee89267ef29e4838899e67
Old-tea <85376439+Old-tea@users.noreply.github.com>
2022-06-03 16:03:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.render('pages/index')
-})
-app.get('/histogram', function(req, res) {
+})*/
+app.get('/', function(req, res) {
     res.sendFile('public/histogram.html', { root: __dirname })
-})
-app.get('/database', function(req, res) {
-    res.sendFile('public/database.html', { root: __dirname })
 })
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
